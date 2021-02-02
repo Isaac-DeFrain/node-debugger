a15126bc99513cf90422c6801aa08606be1a87ce
Isaac-DeFrain <isaacdefrain@gmail.com>
2021-02-01 19:21:02 -0700

    Network_info: include more functionality in Type and improve viewing

---
+++ lib/network_info/type.ml
@@ -0,0 +1,109 @@
+(** Type of network state *)
+
+open Basic
+
+(** parameters: chain *)
+module CMap = Map.Make (Chain)
+
+(** parameters: chain, branch *)
+module CBMap = Map.Make (struct
+  type t = Chain.t * Branch.t
+
+  let compare (c1, b1) (c2, b2) =
+    let c_cmp = compare c1 c2 in
+    if c_cmp < 0 || c_cmp > 0 then c_cmp else compare b1 b2
+end)
+
+(** parameters: chain, node *)
+module CNMap = Map.Make (struct
+  type t = Chain.t * Id.t
+
+  let compare (c1, n1) (c2, n2) =
+    let c_cmp = compare c1 c2 in
+    if c_cmp < 0 || c_cmp > 0 then c_cmp else compare n1 n2
+end)
+
+type t = {
+  mutable active : Id.t list CMap.t;
+  mutable blocks : Blocks.t CBMap.t;
+  mutable branch : Branch.t CMap.t;  (** highest branch on chain *)
+  mutable chain : Chain.t;  (** hightest chain *)
+  mutable height : int CBMap.t;
+  mutable sent : Messages.t CNMap.t;
+  mutable sysmsgs : Message.t Queue.t CMap.t;
+  trace : Execution.t;
+  nodes : Id.t array;
+}
+
+(* chain 1 and branch 0 exist upon creation *)
+let init nodes =
+  let branch = Branch.id 0 in
+  let chain = Chain.id 1 in
+  {
+    active = CMap.(add chain [] empty);
+    blocks = CBMap.(add (chain, branch) Blocks.empty empty);
+    branch = CMap.(add chain branch empty);
+    chain;
+    height = CBMap.(add (chain, branch) (-1) empty);
+    sent = CNMap.empty;
+    sysmsgs = CMap.(add chain (Queue.create ()) empty);
+    trace = Execution.init ();
+    nodes;
+  }
+
+let sys = Id.id 0
+
+(** active nodes on [chain] *)
+let active info chain = try CMap.find chain info.active with Not_found -> []
+
+(** all blocks on [branch] of [chain] *)
+let blocks info chain branch =
+  try CBMap.find (chain, branch) info.blocks with Not_found -> Blocks.empty
+
+let blocks_list info chain branch = blocks info chain branch |> Blocks.to_list
+
+(** current branch on [chain] *)
+let current_branch info chain =
+  try CMap.find chain info.branch with Not_found -> Branch.id (-1)
+
+(** list of all branches on [chain] *)
+let branches info chain =
+  let open List in
+  let b = Branch.to_int (current_branch info chain) in
+  let rec range a b acc =
+    if b < 0 then [] else if a > b then rev acc else range (a + 1) b (a :: acc)
+  in
+  map Branch.id (range 0 b [])
+
+(** height on [branch] of [chain] *)
+let current_height info chain branch =
+  try CBMap.find (chain, branch) info.height with Not_found -> -1
+
+(** list of all heights on [branch] of [chain] *)
+let heights info chain branch =
+  let h = current_height info chain branch in
+  let rec range a b = if a = b then [b] else a :: range (a + 1) b in
+  if h < 0 then [] else range 0 h
+
+(** list of messages sent to [node] on [chain], order is not important *)
+let sent info chain node =
+  try CNMap.find (chain, node) info.sent with Not_found -> Messages.empty
+
+let sent_list info chain node = sent info chain node |> Messages.to_list
+
+let sent_sys info chain = sent info chain sys
+
+(** list of system messages on [chain], order is not important *)
+let sysmsgs info chain =
+  try CMap.find chain info.sysmsgs with Not_found -> Queue.create ()
+
+let sysmsgs_list info chain = sysmsgs info chain |> Queue.to_list
+
+(** list of all chains *)
+let chains info =
+  let open Chain in
+  let c = to_int info.chain in
+  if c = 1 then [id c]
+  else
+    let rec range a b = if a = b then [b] else a :: range (a + 1) b in
+    List.map id (range 1 c)
---
+++ lib/network_info/view.ml
@@ -0,0 +1,103 @@
+(** String representation of network state *)
+
+open Basic
+open Type
+open Printf
+
+let view_list viewer l =
+  sprintf "[%s]" @@ String.concat ", " @@ List.map viewer l
+
+(* viewing functions *)
+let view_active info chain = view_list Id.view @@ active info chain
+
+let view_blocks info chain branch =
+  view_list Block.view @@ blocks_list info chain branch
+
+let view_branch info chain = Branch.view @@ current_branch info chain
+
+let view_branches info chain =
+  view_list Branch.view @@ List.sort Branch.compare @@ branches info chain
+
+let view_chains info = view_list Chain.view @@ chains info
+
+let view_nodes info = view_list Id.view @@ Array.to_list info.nodes
+
+let view_height info chain branch =
+  string_of_int @@ current_height info chain branch
+
+let view_heights info chain branch =
+  view_list string_of_int @@ heights info chain branch
+
+let view_sent info chain node =
+  view_list Message.view @@ sent_list info chain node
+
+let view_sysmsgs info chain = view_list Message.view @@ sysmsgs_list info chain
+
+let view_chain_blocks ?(sp = 2) info chain =
+  let branch_id b = String.make sp ' ' ^ Branch.view b ^ " :> " in
+  String.concat_endline
+  @@ List.map (fun b -> branch_id b ^ view_blocks info chain b)
+  @@ branches info chain
+
+let view_chain_branches info chain =
+  if branches info chain = [] then "" else view_branches info chain
+
+let view_chain_heights ?(sp = 2) info chain =
+  let branch_id b = String.make sp ' ' ^ Branch.view b ^ " :> " in
+  String.concat_endline
+  @@ List.map (fun b -> branch_id b ^ view_heights info chain b)
+  @@ branches info chain
+
+let view_chain_sent ?(sp = 2) info chain =
+  let node_id n = String.make sp ' ' ^ Id.view n ^ " :> " in
+  String.concat_endline
+  @@ List.map (fun n -> node_id n ^ view_sent info chain n)
+  @@ active info chain
+
+let view_chain_sysmsgs info chain =
+  let open Queue in
+  let sysmsgs = sysmsgs info chain in
+  if is_empty sysmsgs then "" else view_sysmsgs info chain
+
+(* view of a single [chain] *)
+let view_chain info chain =
+  String.concat_endline
+    [ "chain: " ^ Chain.view chain;
+      "active: " ^ view_active info chain;
+      "blocks:\n" ^ view_chain_blocks info chain;
+      "branch: " ^ view_chain_branches info chain;
+      "height:\n" ^ view_chain_heights info chain;
+      "sent:\n" ^ view_chain_sent info chain;
+      "sysmsgs: " ^ view_chain_sysmsgs info chain ]
+
+let state_viewer info viewer =
+  let chain_id c = String.make 2 ' ' ^ Chain.view c ^ " :> " in
+  String.concat_endline
+  @@ List.map (fun c -> chain_id c ^ viewer info c)
+  @@ chains info
+
+let view_state_active info = state_viewer info view_active
+
+let view_state_blocks info = state_viewer info @@ view_chain_blocks ~sp:4
+
+let view_state_branch info = state_viewer info view_chain_branches
+
+let view_state_heights info = state_viewer info @@ view_chain_heights ~sp:4
+
+let view_state_sent info = state_viewer info @@ view_chain_sent ~sp:4
+
+let view_state_sysmsgs info = state_viewer info view_chain_sysmsgs
+
+(* view of complete state of the network *)
+let view info =
+  String.concat_endline
+    [ "chains: " ^ view_chains info;
+      "nodes:  " ^ view_nodes info;
+      "active:\n" ^ view_state_active info;
+      "blocks:\n" ^ view_state_blocks info;
+      "branch:\n" ^ view_state_branch info;
+      "height:\n" ^ view_state_heights info;
+      "sent:\n" ^ view_state_sent info;
+      "sysmsgs:\n" ^ view_state_sysmsgs info ]
+
+let view_trace t = Execution.view t.trace
